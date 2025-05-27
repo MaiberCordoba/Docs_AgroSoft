17ac9111d399a39b7419fab62716cfb6282a3c64
maiberCordoba <cordobamaiber@gmail.com>
2025-05-27 18:09:51 -0500

    proyecto base paradocumentaciones del sistema AgroSoft

---
+++ docs_Agrosoft/astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Docs AgroSoft",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Planteamiento del problema",
+          items: [{ label: "Example Guide", slug: "doc1/example" }],
+        },
+        {
+          label: "Especificacion Requisitos de Software",
+          items: [{ label: "Example Guide", slug: "doc2/example" }],
+        },
+        {
+          label: "Prototipado del Sistema",
+          items: [{ label: "Example Guide", slug: "doc3/example" }],
+        },
+        {
+          label: "Manual de Configuracion Base de Datos",
+          items: [{ label: "Example Guide", slug: "doc4/example" }],
+        },
+      ],
+    }),
+  ],
+});
